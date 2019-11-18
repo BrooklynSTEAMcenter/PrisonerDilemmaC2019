58a32befcebce3ca27b61d788760611a72ee796b
davonna1 <57914102+davonna1@users.noreply.github.com>
2019-11-18 14:58:38 -0500

    Add files via upload

--- team0.py
+++ team0.py
@@ -6,16 +6,16 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team 0' # Only 10 chars displayed.
+strategy_name = 'Betray'
+strategy_description = 'Always Betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    Make my move. Make move based on history with this partner.
+    Returns 'b'.
     '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
@@ -26,13 +26,13 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
     from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
+    Returns True or False, depending on whether result was as expected.
     '''
     real_result = move(my_history, their_history, my_score, their_score)
     if real_result == result:

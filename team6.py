1ac195816ffb2763dc9b41968dc838c6f7d48091
junior-jean <57913962+junior-jean@users.noreply.github.com>
2019-11-18 15:01:35 -0500

    Add files via upload

--- team6.py
+++ team6.py
@@ -5,10 +5,11 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
+from random import choice
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = "Religion"
+strategy_name = "Jebaited"
+strategy_description = "Every third it's always betray, everything else is random"
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -18,6 +19,7 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
+
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,8 +27,28 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+
+    #Keep track of their history to see if they betrayed us three times in a row
+
+    choices = ["b", "c"]
+    countOfBetrays = 0
+    theyBetrayed = None
+
+    if len(their_history) >= 3:
+        i = -1
+        while i > -4:
+            if their_history[i] == 'b':
+                countOfBetrays += 1
+            if countOfBetrays == 3:
+             countOfBetrays = 0
+
+            theyBetrayed = True
+            i -= 1
+
+    if len(my_history) % 7 == 0 or theyBetrayed == True:
+        return 'b'
+    else:
+        return choice(choices)
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -48,8 +70,8 @@ def test_move(my_history, their_history, my_score, their_score, result):
 if __name__ == '__main__':
      
     # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
+    if test_move(my_history='cccc',
+              their_history='cbbb',
               my_score=0,
               their_score=0,
               result='b'):
